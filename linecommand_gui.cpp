34d1b1b0ebc90bcd7aec69baaed0527d26979858
Maxonaldo <maxapfc@gmail.com>
2015-05-31 22:29:19 +0200

    -

---
+++ linecommand_gui.cpp
@@ -0,0 +1,14 @@
+#include "linecommand_gui.h"
+
+linecommand_gui::linecommand_gui(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::linecommand_gui)
+{
+    ui->setupUi(this);
+}
+
+linecommand_gui::~linecommand_gui()
+{
+
+}
+
